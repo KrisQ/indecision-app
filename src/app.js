9056e29c81a09e04522d8c58663d36a12ce180f4
Kristenn <kquemener@yahoo.com>
2019-02-22 21:11:51 -0600

    count

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 // JSX - Javascrip XML
 
+const obj = {
+  name: "Vikram",
+  getName() {
+    return this.name;
+  }
+};
+
+console.log(obj.getName());
+
 class IndecisionApp extends React.Component {
   render() {
     const title = "Indecision App";
     const subTitle = "Put your life in the hands of a computer";
-    const options = ['thing 1', 'thing 2', 'thing 3']
+    const options = ["thing 1", "thing 2", "thing 3"];
     return (
       <div>
         <Header title={title} subTitle={subTitle} />
@@ -30,21 +39,34 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert("handle pick");
+  }
   render() {
     return (
       <div>
-        <button>What should I do</button>
+        <button onClick={this.handlePick}>What should I do</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll() {
+    alert("remove all");
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         <p>{this.props.options.length}</p>
-        {this.props.options.map((option,index) => <Option key={index} optionText={option} />)}
+        {this.props.options.map((option, index) => (
+          <Option key={index} optionText={option} />
+        ))}
       </div>
     );
   }
@@ -61,9 +83,20 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button type="submit">Add Option</button>
+        </form>
         <p>AddOption Component here</p>
       </div>
     );
