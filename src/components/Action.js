e5eb2e7ac4309e4da3afaeba1a8462b15d3f393b
Kristenn <kquemener@yahoo.com>
2019-02-24 20:23:16 -0600

    modal

--- src/components/Action.js
+++ src/components/Action.js
@@ -11,3 +11,4 @@ const Action = props => {
 }
 
 export default Action
+
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,17 +3,12 @@ import AddOption from './AddOption'
 import Header from './Header'
 import Action from './Action'
 import Options from './Options'
+import OptionModal from './OptionModal'
 
 class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.state = {
-      options: []
-    }
+  state = {
+    options: [],
+    selectedOption: undefined
   }
   componentDidMount() {
     try {
@@ -37,26 +32,29 @@ class IndecisionApp extends React.Component {
   componentWillUnmount() {
     console.log('DESTROY 🔥')
   }
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }))
   }
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = optionToRemove => {
     this.setState(prevState => ({
       options: prevState.options.filter(option => optionToRemove !== option)
     }))
   }
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length)
     const option = this.state.options[randomNum]
-    alert(option)
+    this.setState(() => ({ selectedOption: option }))
   }
-  handleAddOption(option) {
+  handleAddOption = option => {
     if (!option) {
       return 'Enter valid value to add item'
     } else if (this.state.options.indexOf(option) > -1) {
       return 'This option already exists'
     }
     this.setState(prevState => ({ options: [...prevState.options, option] }))
+  }
+  handleCloseModal = () => {
+    this.setState(() => ({ selectedOption: undefined }))
   }
   render() {
     const subtitle = 'Put your life in the hands of a computer'
@@ -74,6 +72,10 @@ class IndecisionApp extends React.Component {
           handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleCloseModal={this.handleCloseModal}
+        />
       </div>
     )
   }
