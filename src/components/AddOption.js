5f1175fa1df0d6f78091bc6b8dc157fcd38ec617
Kristenn <kquemener@yahoo.com>
2019-02-28 23:01:52 -0600

    hooks fun

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,52 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Button } from './Styles'
 
-class AddOption extends React.Component {
-  state = {
-    error: undefined
-  }
+const AddOption = props => {
+  const [error, setError] = useState(undefined)
 
-  handleAddOption = e => {
+  const handleAddOption = e => {
     e.preventDefault()
     const option = e.target.elements.option.value.trim()
-    const error = this.props.handleAddOption(option)
-    this.setState(() => ({ error }))
+    const error = props.handleAddOption(option)
+    setError(() => ({ error }))
     if (!error) {
       e.target.elements.option.value = ''
     }
   }
-  render() {
-    const OptionError = styled.p`
-      color: #a5afd7;
-      font-style: italic;
-      padding: 0 0.5rem;
-      margin: 1.5rem;
-    `
-    const AddOption = styled.form`
-      display: flex;
-      padding: 0.5rem;
-    `
+  const OptionError = styled.p`
+    color: #a5afd7;
+    font-style: italic;
+    padding: 0 0.5rem;
+    margin: 1.5rem;
+  `
+  const AddOption = styled.form`
+    display: flex;
+    padding: 0.5rem;
+  `
 
-    const GrowInput = styled.input`
-      background: #333745;
-      border: none;
-      border-bottom: .3rem solid #333;
-      flex-grow: 1;
-      margin-right: .5rem;
-      color: ghostwhite;
-      padding: .4rem;
-    `
-    return (
-      <div>
-        {this.state.error && <OptionError>{this.state.error}</OptionError>}
-        <AddOption onSubmit={this.handleAddOption}>
-          <GrowInput type="text" name="option" />
-          <Button>Add Option</Button>
-        </AddOption>
-      </div>
-    )
-  }
+  const GrowInput = styled.input`
+    background: #333745;
+    border: none;
+    border-bottom: 0.3rem solid #333;
+    flex-grow: 1;
+    margin-right: 0.5rem;
+    color: ghostwhite;
+    padding: 0.4rem;
+  `
+  return (
+    <div>
+      {error && <OptionError>{error}</OptionError>}
+      <AddOption onSubmit={handleAddOption}>
+        <GrowInput type="text" name="option" />
+        <Button>Add Option</Button>
+      </AddOption>
+    </div>
+  )
 }
 
 export default AddOption
